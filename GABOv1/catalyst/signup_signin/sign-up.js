76800cd4d59568b7c48d5c7359625397f8e1b869
Stephen <perdigon.johnstephen.bsit2020@gmail.com>
2023-11-05 23:33:29 +0800

    update

--- GABOv1/catalyst/signup_signin/sign-up.js
+++ GABOv1/catalyst/signup_signin/sign-up.js
@@ -27,3 +27,44 @@ document.querySelector('.show-password').addEventListener('click', function() {
     }
   });
 
+
+function showAlert(message, alertType) {
+  const alertContainer = document.getElementById('alertContainer');
+
+  const alertElement = document.createElement('div');
+  alertElement.classList.add('alert', 'alert-dismissible', 'fade', 'show', `alert-${alertType}`);
+  alertElement.setAttribute('role', 'alert');
+
+  alertElement.innerHTML = `
+      ${message}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  `;
+
+  alertContainer.appendChild(alertElement);
+
+  // Automatically remove the alert after a few seconds (optional)
+  setTimeout(() => {
+      alertElement.remove();
+  }, 5000); // 5000 milliseconds = 5 seconds
+}
+
+// Email Validation and Auto format
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  // Get the input value
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value;
+
+  // Regular expression for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(emailValue)) {
+      // Valid email format
+      showAlert('Email is valid!', 'success');
+  } else {
+      // Invalid email format
+      showAlert('Please enter a valid email address.', 'danger');
+  }
+});
