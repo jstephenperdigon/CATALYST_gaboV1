a7d10123b19bae49d7536a8c366decba6f272357
xealv <yrafontanilla14@gmail.com>
2023-12-11 13:10:08 +0800

    monitor index reports 60 percent

---
+++ catalyst/monitoring_center/monitor-index-reports.js
@@ -0,0 +1,145 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getDatabase, ref, get, onValue } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAsGSaps-o0KwXTF-5q3Z99knmyXPmSfU",
+    authDomain: "smartgarbagebin-8c3ec.firebaseapp.com",
+    databaseURL: "https://smartgarbagebin-8c3ec-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smartgarbagebin-8c3ec",
+    storageBucket: "smartgarbagebin-8c3ec.appspot.com",
+    messagingSenderId: "1062286948871",
+    appId: "1:1062286948871:web:d62f6f620e010f8f22c8a2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+// Function to generate the HTML for a single report
+function generateReportHTML(report) {
+    return `
+        <tr>
+            <td>${report.ticketNumber}</td>
+            <td>${report.gcn}</td>
+            <td>${report.Date}</td>
+            <td>${report.timeFormat12}</td>
+            <td>${report.Problem}</td>
+            <td>${report.Description}</td>
+            <td>${report.addressLine1}</td>
+            <td>${report.addressLine2}</td>
+            <td>${report.barangay}</td>
+            <td>${report.district}</td>
+            <td>${report.firstName} ${report.lastName}</td>
+            <td>${report.email}</td>
+            <td>${report.mobileNumber}</td>
+            <td>
+                <!-- Action buttons go here -->
+                <button onclick="performAction('edit', '${report.ticketNumber}')">Edit</button>
+                <button onclick="performAction('delete', '${report.ticketNumber}')">Delete</button>
+                <button onclick="performAction('details', '${report.ticketNumber}')">Details</button>
+            </td>
+        </tr>
+    `;
+}
+
+// Function to filter reports based on search input and selected sorting column
+function filterReports(searchInput, sortKey) {
+    const reportsArray = document.querySelectorAll('#reportsTable tbody tr');
+    reportsArray.forEach(report => {
+        const columnValue = report.querySelector(`td:nth-child(${getIndex(sortKey)})`).textContent.toLowerCase();
+        const displayStyle = columnValue.includes(searchInput) ? '' : 'none';
+        report.style.display = displayStyle;
+    });
+}
+
+// Modify the searchReports function to use the filterReports function
+window.searchReports = function () {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const sortKey = document.getElementById('sortDropdown').value;
+
+    filterReports(searchInput, sortKey);
+};
+
+// Placeholder function for performing actions on report
+window.performAction = function (action, ticketNumber) {
+    // Placeholder function, you can implement the actual actions here
+    console.log(`Performing ${action} for ticket #${ticketNumber}`);
+};
+
+// Function to get the index of the selected column
+function getIndex(key) {
+    const headers = ['ticketNumber', 'gcn', 'Date', 'timeFormat12', 'Problem', 'Description', 'addressLine1', 'addressLine2', 'barangay', 'district', 'Name', 'email', 'mobileNumber', 'action'];
+    return headers.indexOf(key) + 1;
+}
+
+// Function to display the reports table
+function displayReportsTable(reportsArray) {
+    // Sort reports by date and time initially
+    reportsArray.sort((a, b) => {
+        const dateA = new Date(`${a.Date} ${a.timeFormat12}`);
+        const dateB = new Date(`${b.Date} ${b.timeFormat12}`);
+        return dateA - dateB;
+    });
+
+    const reportsTable = document.getElementById('reportsTable');
+    const tableHTML = `
+        <table border="2">
+            <thead>
+                <tr>
+                    <th>Ticket #</th>
+                    <th>GCN</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Problem</th>
+                    <th>Description</th>
+                    <th>Address Line 1</th>
+                    <th>Address Line 2</th>
+                    <th>Barangay</th>
+                    <th>District</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile Number(+63)</th>
+                    <th>Action</th><!-- New column for Action buttons -->
+                </tr>
+            </thead>
+            <tbody>
+                ${reportsArray.map(generateReportHTML).join('')}
+            </tbody>
+        </table>
+    `;
+    reportsTable.innerHTML = tableHTML;
+}
+
+// Function to update the table when data changes
+function updateTable() {
+    const reportsRef = ref(db, 'Reports');
+    onValue(reportsRef, (snapshot) => {
+        const reportsData = snapshot.val();
+        if (reportsData) {
+            const reportsArray = Object.entries(reportsData).map(([ticketNumber, report]) => ({ ticketNumber, ...report }));
+            displayReportsTable(reportsArray);
+        } else {
+            displayReportsTable([]);
+        }
+    });
+}
+
+// Function to reset the list
+window.resetList = function () {
+    // Clear the search input
+    document.getElementById('searchInput').value = '';
+
+    // Reset the sort dropdown to the default option
+    document.getElementById('sortDropdown').selectedIndex = 0;
+
+    // Retrieve the initial data and update the table
+    updateTable();
+};
+
+// Display the initial reports table when the page loads
+window.onload = function () {
+    updateTable();
+};
