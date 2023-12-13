c72ac25e95b818430a172a8f352f278c507df841
Stephen <perdigon.johnstephen.bsit2020@gmail.com>
2023-12-13 16:44:55 +0800

    Monitor Index

--- catalyst/monitoring_center/monitor-index-design.js
+++ catalyst/monitoring_center/monitor-index-design.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-    // Handle click events on navigation links
-    $('.nav-link').click(function (e) {
-      e.preventDefault();
-      var targetId = $(this).data('target');
+  // Handle click events on navigation links
+  $(".nav-link").click(function (e) {
+    e.preventDefault();
+    var targetId = $(this).data("target");
 
-      // Hide all content divs
-      $('#reports, #summary').removeClass('active-content').addClass('hidden');
+    // Hide all content divs
+    $("#reports, #summary").removeClass("active-content").addClass("hidden");
 
-      // Show the selected content
-      $('#' + targetId).removeClass('hidden').addClass('active-content');
-    });
+    // Show the selected content
+    $("#" + targetId)
+      .removeClass("hidden")
+      .addClass("active-content");
   });
+});
